18977f0f58fcd64f9da8fe486911efe00ce0c3e5
Waranya60160215 <mintly.41@hotmail.com>
2020-11-07 17:37:44 +0700

    commit edit item

--- tests/test.js
+++ tests/test.js
@@ -16,4 +16,14 @@ describe('GET "/"', () => {
 
 })
 
+describe('GET "/item"', () => {
+    it('respond with array of item', (done) => {
+        let want = [
+            {id: 1, name:'iphone 12 Pro Max'},
+            {id: 2, name:'Google Pixel 5'}
 
+        ]
+        request(app).get('/item').expect(want, done)
+    })
+
+})
