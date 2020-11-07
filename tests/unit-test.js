f9885d7081fa8207a878d4814747356e39699ffa
Waranya60160215 <mintly.41@hotmail.com>
2020-11-07 17:48:46 +0700

    add unit test

---
+++ tests/unit-test.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../index')
+
+describe('GET "/item"', () => {
+    it('respond with array of item', (done) => {
+        let want = [
+            {id: 1, name:'iphone 12 Pro Max'},
+            {id: 2, name:'Google Pixel 5'}
+
+        ]
+        request(app).get('/item').expect(want, done)
+    })
+})
